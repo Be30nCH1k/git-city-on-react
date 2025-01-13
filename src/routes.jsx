4a08b530b2d3ee08ad0206f9607de1a9773d7643
be30nch1k <danildudin592@gmail.com>
2025-01-13 14:44:00 +0300

    testNumberFour

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,26 +3,39 @@ import HomePage from './pages/HomePage/HomePage';
 import ContactPage from './pages/ContactPage/ContactPage';
 import AttractionPage from './pages/AttractionsPage/AttractionsPage';
 import AttractionDetail from './components/AttractionsDetail/AttractionsDetail';
+import ErrorPage from './pages/ErrorPage/ErrorPage'; // Компонент для отображения ошибок
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+      errorElement: <ErrorPage />, // Обработка ошибок для этого маршрута
+    },
+    {
+      path: '/contact',
+      element: <ContactPage />,
+      errorElement: <ErrorPage />, // Обработка ошибок для этого маршрута
+    },
+    {
+      path: '/attractions',
+      element: <AttractionPage />,
+      errorElement: <ErrorPage />, // Обработка ошибок для этого маршрута
+    },
+    {
+      path: '/attractions/id/:id',
+      element: <AttractionDetail />,
+      errorElement: <ErrorPage />, // Обработка ошибок для этого маршрута
+    },
+    {
+      path: '*', // Ловит все несуществующие маршруты
+      element: <ErrorPage />, // Отображает кастомную страницу 404
+    },
+  ],
   {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/contact',
-    element: <ContactPage />,
-  },
-  {
-    path: '/attractions',
-    element: <AttractionPage />,
-  },
-  {
-    path: '/attractions/id/:id',
-    element: <AttractionDetail />,
+    basename: process.env.PUBLIC_URL || '',
   }
-  
-]);
+);
 
 export default function Routes() {
   return <RouterProvider router={router} />;
